611a44255c853c4711f5a5dc90a3ae87f8a1bd84
Diksha Sharma <diksha66sharma78@gmail.com>
2024-11-25 15:05:17 -0500

    Question1 complete

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Take an array as input from user
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the size of your array");
+        int size = sc.nextInt();
+        int [] numbers = new int[size];
+
+        for(int i =0; i<size; i++){
+            numbers [i] = sc.nextInt();
+        }
+
+        //Search for give number x
+        System.out.println("Enter the number you want to search");
+        int x = sc.nextInt();
+        //print the index at which it occurs.
+        for(int j =0; j<size; j++){
+            if(numbers[j] == x){
+                System.out.println(j);
+            }
+        }
+    }
+}
