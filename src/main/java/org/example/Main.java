d57d956b5e1acb9d86189dfd28ce8f0408d7c819
Diksha Sharma <diksha66sharma78@gmail.com>
2024-12-03 15:19:35 -0500

    longest substring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,18 @@ import org.Array1;
 
 public class Main {
     public static void main(String[] args) {
-        Array1 arr = new Array1();
-        arr.linearSearch();
+        // Array1 arr = new Array1();
+        // arr.linearSearch();
 
-        Array2 arr2 = new Array2();
-        arr2.multiDimension();
+        // Array2 arr2 = new Array2();
+        // arr2.multiDimension();
 
-        Strings string = new Strings();
-        string.StringReverse();
+        // Strings string = new Strings();
+        // string.StringReverse();
+
+        commonString str = new commonString();
+        String result = str.longestSubstring("abcdef", "zabxcdef");
+        System.out.println(result);
         
         
     }
---
+++ src/main/java/org/example/commonString.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class commonString {
+
+    // Find the longest common substring between two strings.
+    public String longestSubstring(String str1, String str2){
+        int m = str1.length();
+        int n = str2.length();
+        int[][] dp = new int[m + 1][n + 1]; // DP table to store lengths of common substrings
+        int maxLength = 0; // Maximum length of common substring
+        int endIndex = 0; // Ending index of the longest common substring in str1
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    if (dp[i][j] > maxLength) {
+                        maxLength = dp[i][j];
+                        endIndex = i - 1;
+                    }
+                }
+            }
+        }
+
+        return str1.substring(endIndex - maxLength + 1, endIndex + 1);
+    }
+
+
+
+}
+
+
+
