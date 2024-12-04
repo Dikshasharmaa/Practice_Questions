822521e511ca8253ad38488c7c8a76168820c5b1
Diksha Sharma <diksha66sharma78@gmail.com>
2024-12-03 21:44:18 -0500

    Reversing a list through iterative way

--- src/main/java/org/example/Collections.java
+++ src/main/java/org/example/Collections.java
@@ -1,6 +1,8 @@
 package org.example;
 import java.util.*;
 
+import org.w3c.dom.Node;
+
 public class Collections {
 
     public void LinkList(){
@@ -11,8 +13,25 @@ public class Collections {
         list.add(1,"my");
         list.remove(0);
 
-        System.out.println(list);
+        System.out.println(list);   
+
     }
+// Reverse link list through iterative way
+    // public void reverselist(){
+    //     Node preNode = head;
+    //     Node currNode = head.next;
+    //     while(currNode !=null){
+    //         Node newNode = currNode.next;
+    //         currNode.next = prev;
+    //         prevNode = currNode;
+    //         currNode = newNode;
+
+    //     }
+    //     head.next = null;
+    //     head = preNode;
+    // }
+
+    // }
 
 
 }
